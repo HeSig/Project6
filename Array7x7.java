27be63bbb2d07ddaf814819456f33f12b8e61dae
jakobkennerberg <34630826+jakobkennerberg@users.noreply.github.com>
2018-01-03 15:32:32 +0100

    Add files via upload

--- Array7x7.java
+++ Array7x7.java
@@ -1,71 +1,68 @@
 package p6;
 
 public class Array7x7 {
-	private int[][] array2x;
-	private Array7 array7;
+	private Array7[] array7x7 = new Array7[7];
 	
 	public Array7x7() {
-		this.array2x = new int[7][7];
-	}
-	
-	public Array7x7(Array7x7 array7x7) {
-		this.array2x = new int[7][7];
-		setArray(array7x7);
-	}
-	
-	public Array7x7(int[][] array2x) {
-		this.array2x = new int[7][7];
-		setArray(array2x);
-	}
-	
-	public void setElement(int row, int col, int value) {
-		this.array2x[row][col] = value;
-	}
-	
-	public int getElement(int row, int col) {
-		return array2x[row][col];
-	}
-	
-	public void setRow(int row, Array7 theRow) {
-		for(int i = 0; i < array2x.length; i++) {
-			this.array2x[row][i] = theRow.getElement(i);
+		for(int i = 0; i < 7; i++) {
+			array7x7[i]  = new Array7();
 		}
 	}
 	
+	public Array7x7(int[][] array) {
+		for(int i = 0; i < 7; i++) {
+			array7x7[i] = new Array7();
+		}
+		for(int row = 0; row < array7x7.length; row++) {
+			for(int col = 0; col < array7x7.length; col++) {
+				array7x7[row].setElement(col, array[row][col]);
+			}
+		}
+	}
+	
+	public void setElement(int row, int col, int value) {
+		array7x7[row].setElement(col, value);
+	}
+	
+	public int getElement(int row, int col) {
+		return array7x7[row].getElement(col);
+	}
+	
+	public void setRow(int row, Array7 theRow) {
+		array7x7[row] = theRow;
+	}
+	
 	public Array7 getRow(int row) {
-		Array7 rowarray = new Array7();
-		rowarray.setArray(array2x[row]);
-		return rowarray;
-		
+		return array7x7[row];	
 	}
 	
 	public void setCol(int col, Array7 theCol) {
-		for(int i = 0; i < array2x[col].length; i++) {
-			this.array2x[i][col] = theCol.getElement(i);
+		for(int i = 0; i < array7x7.length; i++) {
+			array7x7[i].setElement(col, theCol.getElement(i) );
 		}
 	}
 	
 	public Array7 getCol(int col) {
 		Array7 colarray = new Array7();
 		
-		for(int i = 0; i < array2x[i].length; i++) {
-			colarray.setElement(i, array2x[i][col]);
+		for(int i = 0; i < array7x7.length; i++) {
+			colarray.setElement(i, array7x7[i].getElement(col));
 		}
 		return colarray;
 	}
 	
-	public void setArray(Array7x7 array7x7) {
-		for(int row = 0; row < array2x.length; row++) {
-			for(int col = 0; col < array2x[row].length; col++) {
-				this.array2x[row][col] = array7x7.getElement(row, col);
+	public void setArray(Array7x7 array) {
+		for(int row = 0; row < array7x7.length; row++) {
+			for(int col = 0; col < array7x7.length; col++) {
+				array7x7[row].setElement(col, array.getElement(row, col));
 			}
 		}
 	}
 	
-	public void setArray(int[][] array2x) {
-		for(int row = 0; row < array2x.length; row++) {
-			for(int col = 0; col < array2x[row].length; col++) {
-				this.array2x[row][col] = array2x[row][col];
+	public void setArray(int[][] array) {
+		for(int row = 0; row < array7x7.length; row++) {
+			for(int col = 0; col < array7x7.length; col++) {
+				array7x7[row].setElement(col, array[row][col]);
 			}
 		}
 	}
@@ -75,6 +72,12 @@ public class Array7x7 {
 	}
 	
 	public int[][] toIntArray() {
-		return new int[7][7];
+		int[][] newArray7x7 = new int[7][7];
+		for(int row = 0; row < 7; row++) {
+			for(int col = 0; col < 7; col++) {
+				newArray7x7[row][col]=array7x7[row].getElement(col);
+			}
+		}
+		return newArray7x7;
 	}
 }
